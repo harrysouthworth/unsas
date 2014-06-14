77f78e84240ec9328160f760e9c608634a8ee61e
Harry <harry.southworth@gmail.com>
2014-06-14 14:07:49 +0100

    got it working -ish

--- sasparser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~/Work/repos/github/unsas/
-java -cp lib/*:bin com.puskas.sasparser.main.fileOps $1
---
+++ unsas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Work/repos/github/unsas/
+java -cp lib/*:bin com.dcc.unsas.main.unsas $PWD/$1

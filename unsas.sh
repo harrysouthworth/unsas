a6e7c3e52d5fe7916efc63ce6d6aaf07ae74b4aa
harry <harry@dataclarityconsulting.co.uk>
2015-03-18 09:20:51 +0000

    Reverting to a version that works. unsas.sh needs libs, not lib, buecause that's the correct directory (again)

--- unsas.sh
+++ unsas.sh
@@ -2,6 +2,6 @@
 #$wh=$PWD/$1
 cd ~/Work/repos/github/unsas/
 #echo $wh
-java -cp lib/*:bin com.dcc.unsas.main.unsas $1
+java -cp libs/*:bin com.dcc.unsas.main.unsas $1
 python3 python/cleanCSV.py "$1/csv"
 

9c74042525759f0ba6eb3a603b5333ed95cdeff0
Harry Southworth <harry.southworth@gmail.com>
2015-07-27 09:11:50 +0100

    Adding functionality: now a new directory csv/meta is created and the file meta data (column descriptions) are written to files in there

--- src/com/dcc/unsas/main/unsas.java
+++ src/com/dcc/unsas/main/unsas.java
@@ -15,6 +15,7 @@ import java.io.Writer;
 import com.ggasoftware.parso.CSVDataWriter;
 import com.ggasoftware.parso.CSVMetadataWriter;
 import com.ggasoftware.parso.SasFileReader;
+//import com.ggasoftware.parso.StringWriter;
 
 
 class unsas {
@@ -26,11 +27,18 @@ class unsas {
 
     /* Create csv directory */
     String csv = args[0] + "/csv";
+    String meta = csv + "/meta/";
     System.out.println(csv);
+
     Boolean success = (new File(csv)).mkdirs();
     if (!success){
       System.out.println("Failed to create csv directory");
       System.exit(1);
+    }
+    success = (new File(meta)).mkdirs();
+    if (!success){
+    	System.out.println("Failed to create metadata directory");
+    	System.exit(1);
     }
 
     /* Get names of SAS files in the path */
@@ -40,9 +48,10 @@ class unsas {
     for (String fn : fns){
       String inf = args[0] + "/" + fn + ".sas7bdat";
       String ouf = args[0] + "/csv/" + fn + ".csv";
+      String mdf = args[0] + "/csv/meta/" + fn + ".csv";
 
       System.out.println(fn + ".sas7bdat -> csv/" + fn + ".csv");
-      createDataFile(inf, ouf);
+      createDataFile(inf, ouf, mdf);
     }
   }
 
@@ -81,17 +90,23 @@ class unsas {
   return(sasfiles);
   } /* Close getSasFilenames */
 
-  private static void createDataFile(String sasfile, String csvfile) {
+  private static void createDataFile(String sasfile, String csvfile, String metafile) {
     try {
       File file = new File(sasfile);
 
       FileInputStream fis = new FileInputStream(file);
       SasFileReader sasFileReader = new SasFileReader(fis);
 
+      // Write the data
       Writer writer = new FileWriter(csvfile);
       CSVDataWriter csvDataWriter = new CSVDataWriter(writer);
       csvDataWriter.writeColumnNames(sasFileReader.getColumns());
       csvDataWriter.writeRowsArray(sasFileReader.getColumns(), sasFileReader.readAll());
+      
+      // Write the metadata
+      Writer mwriter = new FileWriter(metafile);
+      CSVMetadataWriter csvMetadataWriter = new CSVMetadataWriter(mwriter);
+      csvMetadataWriter.writeMetadata(sasFileReader.getColumns());
     }
     catch (IOException ioe) {
       System.err.println("There has been an IO error");
